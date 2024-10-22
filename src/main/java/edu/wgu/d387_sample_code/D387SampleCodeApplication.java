496675f8dde18002f9764d9beee819bf57b8de6f
ChavezCode <jch1248@wgu.edu>
2024-10-21 19:45:13 -0700

    Still working on conversions

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -8,8 +8,12 @@ import org.springframework.context.annotation.ComponentScan;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLOutput;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Properties;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class D387SampleCodeApplication {
@@ -19,6 +23,9 @@ public class D387SampleCodeApplication {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
 		WelcomeMessage welcomeMessage = new WelcomeMessage();
 		welcomeMessage.run();
+
+		TimeZone timeZone = new TimeZone();
+
 
 	}
 
